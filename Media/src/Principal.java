7c91003018f0cd3822a149dc5c27d6a62b93cb09
profesor <profesor@alumnado.flo>
2017-02-09 13:53:40 +0100

    ejercicios sesion 1

--- Media/src/Principal.java
+++ Media/src/Principal.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int acumulado=0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Introduce una nota");
+		int nota = sc.nextInt();
+		acumulado=acumulado+nota;
+		System.out.println("Introduce otra nota");
+		nota = sc.nextInt();
+		System.out.println("Introduce otra nota");
+		nota = sc.nextInt();
+		
+		//Comprobación que las notas están entre 0 y 10
+		
+		if((acumulado/3)>=5)
+			System.out.println("Estás aprobado");
+		else
+			System.out.println("Estás suspendido");
 
 	}
 
